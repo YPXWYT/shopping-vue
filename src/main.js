2917785055a839e873b520cd1f942464e68ea860
YPXWYT <2568824264@qq.com>
2020-05-24 20:57:08 +0800

    :sparkles:图片懒加载vue-lazyload

--- src/main.js
+++ src/main.js
@@ -5,10 +5,19 @@ import App from './App'
 import router from './router'
 import store from './store'
 import {Button} from 'mint-ui'
+
+import VueLazyload from 'vue-lazyload'
+
 import './mock/mockServer' // 加载mockServer即可
+import loading from './common/imgs/loading.gif'
+
+Vue.use(VueLazyload, { // 内部自定义一个指令lazy
+  loading
+})
 
 // 注册全局组件标签
 Vue.component(Button.name, Button)  // <mt-button>
+
 
 new Vue({
   el: '#app',
